40c655aabf97b7a2462d70914713b69cde3fd885
김민지 <ghi512151@gmail.com>
2025-09-22 20:56:29 +0900

    feat: #11 - 기업별 정보 게시물 댓글에 작성자id 컬럼 추가 및 샘플 데이터 수정)

--- ddl/modules/04_career_tables.sql
+++ ddl/modules/04_career_tables.sql
@@ -17,10 +17,12 @@ CREATE TABLE `Career_Info_Comment` (
   `id` INT AUTO_INCREMENT PRIMARY KEY,
   `parent_id` INT NULL,
   `post_id` INT NOT NULL,
+  `member_id` INT NOT NULL,
   `content` VARCHAR(500) NOT NULL,
   `visibility` CHAR(1) NOT NULL DEFAULT 'Y',
   `created_at` VARCHAR(20) NOT NULL,
   `updated_at` VARCHAR(20) NULL,
   CONSTRAINT `FK_Career_Info_Comment_TO_Career_Info_Comment` FOREIGN KEY (`parent_id`) REFERENCES `Career_Info_Comment`(`id`),
-  CONSTRAINT `FK_Career_Info_Post_TO_Career_Info_Comment` FOREIGN KEY (`post_id`) REFERENCES `Career_Info_Post`(`id`)
+  CONSTRAINT `FK_Career_Info_Post_TO_Career_Info_Comment` FOREIGN KEY (`post_id`) REFERENCES `Career_Info_Post`(`id`),
+  CONSTRAINT `FK_Member_TO_Career_Info_Comment` FOREIGN KEY (`member_id`) REFERENCES `Member`(`id`)
 );
