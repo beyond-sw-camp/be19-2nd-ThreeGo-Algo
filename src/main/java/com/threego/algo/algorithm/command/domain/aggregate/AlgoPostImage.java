311142187e73c68bb0e9bcdd14b4662ea77d2ab5
jiruen <kjr2699@naver.com>
2025-09-28 22:45:01 +0900

    feat: #13 - 알고리즘 학습 게시물 이미지 Entity에 생성일시 컬럼 삭제

--- src/main/java/com/threego/algo/algorithm/command/domain/aggregate/AlgoPostImage.java
+++ src/main/java/com/threego/algo/algorithm/command/domain/aggregate/AlgoPostImage.java
@@ -17,16 +17,12 @@ public class AlgoPostImage {
     @Column(name = "image_url", nullable = false)
     private String imageUrl;
 
-    @Column(name = "created_at", nullable = false)
-    private String createdAt;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id", nullable = false)
     private AlgoPost algoPost;
 
     public AlgoPostImage(final String imageUrl, final AlgoPost algoPost) {
         this.imageUrl = imageUrl;
-        this.createdAt = DateTimeUtils.nowDateTime();
         this.algoPost = algoPost;
     }
 }
