1bbad9bcc5fef73969d8a19f688866979657ef14
김민지 <ghi512151@gmail.com>
2025-09-29 18:24:34 +0900

    setting: #1 - 유레카 서버 초기 세팅

---
+++ src/test/java/com/threego/algoeurekaserver/AlgoEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threego.algoeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
