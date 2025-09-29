140fb218d7c591a802d4f6aa5c74f84bad8ba281
Yujin <wjd2000db@gmail.com>
2025-09-29 19:01:07 +0900

    feat: #25 - 관리자용 신고 조회 쿼리 기능 구현

---
+++ src/main/java/com/threego/algo/report/command/domain/aggregate/ReportType.java
@@ -0,0 +1,20 @@
+package com.threego.algo.report.command.domain.aggregate;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Table(name = "Report_Type")
+@NoArgsConstructor
+@Entity
+public class ReportType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String type;
+}
